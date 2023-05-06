18f072db6a98a6833e4cc0f491318b294407b9d4
IamAdren <IamAdren@iamadrenn@gmail.com>
2023-05-06 11:10:20 -0400

    portfolio updates

--- src/pages/world/index.jsx
+++ src/pages/world/index.jsx
@@ -1,3 +1,67 @@
 export const AdrenWorldPage = () => {
-    return <div>Adren World Page</div>
-}
+	return (
+		<div className='text-center' style={{ paddingTop: '85px' }}>
+			<p style={{ fontSize: '28px' }}>
+				What is{' '}
+				<a href='https://www.adren.world' target='_blank'>
+					Adren World
+				</a>
+				?
+			</p>
+
+			<p style={{ fontSize: '24px', width: '650px', margin: 'auto' }}>
+				Adren World is a management company designated to create custom
+				modifications for the GTA-5 Multipleplayer Modification{' '}
+				<a href='https://fivem.net/' target='_blank'>
+					FiveM
+				</a>
+				. Adren World manages 8+ websites, with a combined user count of
+				85,000+ users.
+			</p>
+
+			<p style={{ fontSize: '28px', paddingTop: '25px' }}>Our Brands</p>
+
+			<div style={{ fontSize: '24px' }}>
+				<p>
+					<a target='_blank' href='https://www.adrencad.com'>
+						AdrenCAD
+					</a>{' '}
+					- A Simulated Police Computer Aided Dispatch System.
+				</p>
+
+				<p>
+					<a target='_blank' href='https://www.gbans.xyz'>
+						Global Bans
+					</a>{' '}
+					- FiveM protection management service, and Admin Tools.
+				</p>
+
+				<p>
+					<a target='_blank' href='https://www.fivem.design'>
+						Fivem Designs
+					</a>{' '}
+					- A FiveM Website Creater, similar to Wix or Squarespace.
+				</p>
+
+				<p>
+					<a target='_blank' href='https://www.fivemshare.com'>
+						Fivem Share
+					</a>{' '}
+					- A custom made forums, to share FiveM plugins.
+				</p>
+
+				<p>
+					<a href='#'>FM Monitor</a> - Free and open-source tool too
+					manage FiveM Servers.
+				</p>
+
+				<p>
+					<a target='_blank' href='https://www.adrensbot.com'>
+						Adrens Bot
+					</a>{' '}
+					- A Multi Purpose Discord Bot
+				</p>
+			</div>
+		</div>
+	);
+};
