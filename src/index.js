82d3a7beac045e8ae079d2d45efee79794637be0
Adren <35825183+IamAdren@users.noreply.github.com>
2023-06-01 12:29:50 +0000

    scroll fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
 
 import { Config } from './config';
 
@@ -11,15 +13,41 @@ import App from './App';
 
 import Resume from './pages/resume';
 
+import samplePDF from './certifications/red_cross.pdf';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const ScrollToTop = () => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		document.documentElement.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'instant',
+		});
+	}, [pathname]);
+
+	return null;
+};
 
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<ScrollToTop />
+
 			<Routes>
 				<Route path='/' element={<App />} />
 
 				<Route path='/resume' element={<Resume />} />
+
+				<Route path='/red' element={() => {
+					return (
+						<Document file={samplePDF}>
+							<Page pageNumber={1} />
+						</Document>
+					);
+				}} />
 
 				{Config.projects.map((project) => (
 					<Route
