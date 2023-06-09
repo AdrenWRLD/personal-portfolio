2bf43ac612dab4a619bc5e4f2e544a14cfaeedaf
iamadren <iamadrenn@gmail.com>
2023-06-09 00:43:34 -0400

    java updates'

--- src/pages/java.jsx
+++ src/pages/java.jsx
@@ -1,5 +1,58 @@
 import { useState, useEffect } from 'react';
-import { Button, Card, Container, Spinner } from 'react-bootstrap';
+import { Button, Card, Container, Spinner, Modal } from 'react-bootstrap';
+
+const RepoSourcesModal = ({ show, setShow, files }) => {
+	const handleClose = () => setShow(false);
+
+	return (
+		show &&
+		files && (
+			<Modal
+				show={show}
+				onHide={handleClose}
+				size='lg'
+				style={{ marginTop: '15vh' }}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title>Project Files</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{files.map((file, index) => (
+						<Card
+							body
+							style={{ marginBottom: index == 0 ? '20px' : null }}
+						>
+							<div
+								style={{
+									display: 'flex',
+									alignItems: 'center',
+								}}
+							>
+								<Card.Title>{file.label}</Card.Title>
+
+								<Button
+									variant='secondary'
+									target='_blank'
+									href={file.source}
+									style={{
+										marginLeft: 'auto',
+									}}
+								>
+									Project File
+								</Button>
+							</div>
+						</Card>
+					))}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		)
+	);
+};
 
 export const AdvancedJavaPage = () => {
 	const [Projects, setProjects] = useState(null);
@@ -7,6 +60,10 @@ export const AdvancedJavaPage = () => {
 	const [Fetching, setFetching] = useState(true);
 
 	const [SelectedProject, setSelectedProject] = useState(null);
+
+	const [ProjectFiles, setProjectFiles] = useState(null);
+
+	const [ShowProjectFilesModal, setShowProjectFilesModal] = useState(false);
 
 	useEffect(() => {
 		setFetching(true);
@@ -41,179 +98,237 @@ export const AdvancedJavaPage = () => {
 	}, []);
 
 	return !Fetching ? (
-		SelectedProject ? (
-			<Container style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-				<div style={{ display: 'flex', alignItems: 'center' }}>
-					<h1 className='h2'>{SelectedProject.label}</h1>
+		<div>
+			{ProjectFiles && (
+				<RepoSourcesModal
+					show={ShowProjectFilesModal}
+					setShow={setShowProjectFilesModal}
+					files={ProjectFiles}
+				/>
+			)}
 
-					<a
-						href='/projects/advanced-java'
-						style={{ paddingLeft: '25px' }}
-					>
-						Other Projects
-					</a>
-				</div>
+			{SelectedProject ? (
+				<Container
+					style={{ paddingTop: '30px', paddingBottom: '30px' }}
+				>
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<h1 className='h2'>{SelectedProject.label}</h1>
 
-				<div className='row' style={{ marginBottom: '15px' }}>
-					<div className='col-md-8' />
-
-					<div className='col-md-4 text-center'>
-						<a href='#' style={{ fontSize: '30px' }}>
-							<i className='fas fa-arrow-up'></i>
-						</a>
-					</div>
-				</div>
-
-				<div className='row'>
-					<div className='col-md-8' style={{ minHeight: '420px' }}>
-						<iframe
-							width='100%'
-							height='100%'
-							src={SelectedProject.youtube}
-							title='YouTube video player'
-							frameborder='0'
-							allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-							allowfullscreen
-						></iframe>
-					</div>
-					<div className='col-md-4'>
-						{[
-							SelectedProject.previews[0]
-								? SelectedProject.previews[0]
-								: null,
-							SelectedProject.previews[1]
-								? SelectedProject.previews[1]
-								: null,
-						].map((image) => {
-							return (
-								<img
-									src={image}
-									style={{ width: '100%', cursor: 'pointer' }}
-								/>
-							);
-						})}
-					</div>
-				</div>
-
-				<div className='row' style={{ marginTop: '15px' }}>
-					<div className='col-md-8'>
-						<h1 className='h2' style={{ paddingTop: '15px' }}>
-							Description:
-						</h1>
-
-						<p>
-							Lorem ipsum dolor sit, amet consectetur adipisicing
-							elit. Error maxime nemo corporis dolore facilis
-							optio. Praesentium, repellendus sed natus, pariatur
-							ipsa ratione est ut, consequatur ea voluptatem
-							molestiae cupiditate officiis? Lorem ipsum dolor
-							sit, amet consectetur adipisicing elit. Error maxime
-							nemo corporis dolore facilis optio. Praesentium,
-							repellendus sed natus, pariatur ipsa ratione est ut,
-							consequatur ea voluptatem molestiae cupiditate
-							officiis? Lorem ipsum dolor sit, amet consectetur
-							adipisicing elit. Error maxime nemo corporis dolore
-							facilis optio. Praesentium, repellendus sed natus,
-							pariatur ipsa ratione est ut, consequatur ea
-							voluptatem molestiae cupiditate officiis? Lorem
-							ipsum dolor sit, amet consectetur adipisicing elit.
-							Error maxime nemo corporis dolore facilis optio.
-							Praesentium, repellendus sed natus, pariatur ipsa
-							ratione est ut, consequatur ea voluptatem molestiae
-							cupiditate officiis?
-						</p>
-					</div>
-
-					<div className='col-md-4 text-center'>
-						<a href='#' style={{ fontSize: '30px' }}>
-							<i className='fas fa-arrow-down'></i>
-						</a>
-
-						<hr />
-
-						<Button
-							size='lg'
-							style={{ width: '100%', fontWeight: '700' }}
-							variant='secondary'
-							target='_blank'
-							href={SelectedProject.repo}
+						<a
+							href='/projects/advanced-java'
+							style={{ paddingLeft: '25px' }}
 						>
-							Github Repository
-						</Button>
+							Other Projects
+						</a>
 					</div>
-				</div>
-			</Container>
-		) : (
-			<Container style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-				<h1 className='h2'>Advanced Java Projects</h1>
 
-				<p>Projects</p>
+					<div className='row' style={{ marginBottom: '15px' }}>
+						<div className='col-md-8' />
 
-				<div className='row'>
-					{Projects.map((project) => (
+						<div className='col-md-4 text-center'>
+							<a href='#' style={{ fontSize: '30px' }}>
+								<i className='fas fa-arrow-up'></i>
+							</a>
+						</div>
+					</div>
+
+					<div className='row'>
 						<div
-							className='col-md-4'
-							style={{ marginBottom: '20px' }}
+							className='col-md-8'
+							style={{ minHeight: '420px' }}
 						>
-							<Card body>
-								<a
-									href={
-										'/projects/advanced-java?project=' +
-										project.id
-									}
-								>
-									<Card.Img
-										src={project.previews[0]}
-										style={{ cursor: 'pointer' }}
-									/>
-								</a>
-
-								<Card.Title
-									style={{
-										paddingTop: '15px',
-										fontWeight: '700',
-									}}
-								>
-									{project.label}
-								</Card.Title>
-
-								<hr />
-
-								<a
-									href={
-										'/projects/advanced-java?project=' +
-										project.id
-									}
-								>
-									<Button
+							<iframe
+								width='100%'
+								height='100%'
+								src={SelectedProject.youtube}
+								title='YouTube video player'
+								frameborder='0'
+								allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+								allowfullscreen
+							></iframe>
+						</div>
+						<div className='col-md-4'>
+							{[
+								SelectedProject.previews[0]
+									? SelectedProject.previews[0]
+									: null,
+								SelectedProject.previews[1]
+									? SelectedProject.previews[1]
+									: null,
+							].map((image) => {
+								return (
+									<img
+										src={image}
 										style={{
-											marginTop: '10px',
 											width: '100%',
-											fontWeight: '700',
+											cursor: 'pointer',
 										}}
-									>
-										View Project
-									</Button>
-								</a>
+									/>
+								);
+							})}
+						</div>
+					</div>
 
+					<div className='row' style={{ marginTop: '15px' }}>
+						<div className='col-md-8'>
+							<h1 className='h2' style={{ paddingTop: '15px' }}>
+								Description:
+							</h1>
+
+							<p>
+								Lorem ipsum dolor sit, amet consectetur
+								adipisicing elit. Error maxime nemo corporis
+								dolore facilis optio. Praesentium, repellendus
+								sed natus, pariatur ipsa ratione est ut,
+								consequatur ea voluptatem molestiae cupiditate
+								officiis? Lorem ipsum dolor sit, amet
+								consectetur adipisicing elit. Error maxime nemo
+								corporis dolore facilis optio. Praesentium,
+								repellendus sed natus, pariatur ipsa ratione est
+								ut, consequatur ea voluptatem molestiae
+								cupiditate officiis? Lorem ipsum dolor sit, amet
+								consectetur adipisicing elit. Error maxime nemo
+								corporis dolore facilis optio. Praesentium,
+								repellendus sed natus, pariatur ipsa ratione est
+								ut, consequatur ea voluptatem molestiae
+								cupiditate officiis? Lorem ipsum dolor sit, amet
+								consectetur adipisicing elit. Error maxime nemo
+								corporis dolore facilis optio. Praesentium,
+								repellendus sed natus, pariatur ipsa ratione est
+								ut, consequatur ea voluptatem molestiae
+								cupiditate officiis?
+							</p>
+						</div>
+
+						<div className='col-md-4 text-center'>
+							<a href='#' style={{ fontSize: '30px' }}>
+								<i className='fas fa-arrow-down'></i>
+							</a>
+
+							<hr />
+
+							{SelectedProject.repoSources ? (
 								<Button
-									href={project.repo}
-									target='_blank'
+									size='lg'
+									style={{ width: '100%', fontWeight: '700' }}
 									variant='secondary'
-									style={{
-										marginTop: '10px',
-										width: '100%',
-										fontWeight: '700',
+									onClick={() => {
+										setProjectFiles(
+											SelectedProject.repoSources
+										);
+
+										setShowProjectFilesModal(true);
 									}}
+								>
+									Project Files
+								</Button>
+							) : (
+								<Button
+									size='lg'
+									style={{ width: '100%', fontWeight: '700' }}
+									variant='secondary'
+									target='_blank'
+									href={SelectedProject.repo}
 								>
 									Github Repository
 								</Button>
-							</Card>
+							)}
 						</div>
-					))}
-				</div>
-			</Container>
-		)
+					</div>
+				</Container>
+			) : (
+				<Container
+					style={{ paddingTop: '30px', paddingBottom: '30px' }}
+				>
+					<h1 className='h2'>Advanced Java Projects</h1>
+
+					<p>Projects</p>
+
+					<div className='row'>
+						{Projects.map((project) => (
+							<div
+								className='col-md-4'
+								style={{ marginBottom: '20px' }}
+							>
+								<Card body>
+									<a
+										href={
+											'/projects/advanced-java?project=' +
+											project.id
+										}
+									>
+										<Card.Img
+											src={project.previews[0]}
+											style={{ cursor: 'pointer' }}
+										/>
+									</a>
+
+									<Card.Title
+										style={{
+											paddingTop: '15px',
+											fontWeight: '700',
+										}}
+									>
+										{project.label}
+									</Card.Title>
+
+									<hr />
+
+									<a
+										href={
+											'/projects/advanced-java?project=' +
+											project.id
+										}
+									>
+										<Button
+											style={{
+												marginTop: '10px',
+												width: '100%',
+												fontWeight: '700',
+												marginBottom: '10px',
+											}}
+										>
+											View Project
+										</Button>
+									</a>
+
+									{project.repoSources ? (
+										<Button
+											style={{
+												width: '100%',
+												fontWeight: '700',
+											}}
+											variant='secondary'
+											onClick={() => {
+												setProjectFiles(
+													project.repoSources
+												);
+
+												setShowProjectFilesModal(true);
+											}}
+										>
+											Project Files
+										</Button>
+									) : (
+										<Button
+											style={{
+												width: '100%',
+												fontWeight: '700',
+											}}
+											variant='secondary'
+											target='_blank'
+											href={project.repo}
+										>
+											Github Repository
+										</Button>
+									)}
+								</Card>
+							</div>
+						))}
+					</div>
+				</Container>
+			)}
+		</div>
 	) : (
 		<div className='text-center' style={{ paddingTop: '85px' }}>
 			<Spinner
